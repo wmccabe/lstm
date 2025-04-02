ec46c2313911e51fa2aaaa59ca33b55539e1fcee
Will McCabe <w.m.mccabe@gmail.com>
2025-04-02 12:37:28 -0600

    Refactored multiplies

--- rtl/lstm.sv
+++ rtl/lstm.sv
@@ -63,7 +63,7 @@ module lstm #
 
     logic [DLY - 1 : 0] x_in_valid_dly;
 
-    assign debug = weight_x_reg;
+    assign debug = scaled;
     assign debug_valid = scaled_valid;
 
     // register weights and biases
@@ -98,12 +98,26 @@ module lstm #
         else if (ready && C_in_valid) C_in_reg <= C_in;
         else if (valid)               C_in_reg <= C_out;
     end
+
+    logic signed [WEIGHTS - 1 : 0][31 : 0] product_x;
+    logic signed [WEIGHTS - 1 : 0][31 : 0] product_h;
+    logic signed [WEIGHTS - 1 : 0][31 : 0] rescale_x;
+    logic signed [WEIGHTS - 1 : 0][31 : 0] rescale_h;
+
+    always_comb begin
+        for (int j = 0; j < WEIGHTS; j++) begin
+            product_x[j] = x_in_reg*weight_x_reg[j];
+            product_h[j] = h_in_reg*weight_h_reg[j];
+            rescale_x[j] = product_x[j] >>> 8;
+            rescale_h[j] = product_h[j] >>> 8;
+        end
+    end
         
 
     // implement weighting and scaling
     always_ff @(posedge clk) begin
         for(int j = 0; j < WEIGHTS; j = j + 1) begin 
-            scaled[j] <= (x_in_reg*weight_x_reg[j])/256 + (h_in_reg*weight_h_reg[j])/256 + bias_x_reg[j] + bias_h_reg[j];
+            scaled[j] <= rescale_x[j][WIDTH-1 : 0] + rescale_h[j][WIDTH-1 : 0] + bias_x_reg[j] + bias_h_reg[j];
         end
     end
 
@@ -151,14 +165,30 @@ module lstm #
     end
     endgenerate
 
+    logic signed [31 : 0] product_fC;
+    logic signed [31 : 0] product_ig;
+    logic signed [31 : 0] product_oCtan;
+    logic signed [31 : 0] rescale_fC;
+    logic signed [31 : 0] rescale_ig;
+    logic signed [31 : 0] rescale_oCtan;
+
+    always_comb begin
+        product_fC = activated_dly[f]*C_in_reg;
+        product_ig = activated_dly[i]*activated_dly[g];
+        product_oCtan = activated_dly[o]*C_out_tanh;
+        rescale_fC = product_fC >>> 8;
+        rescale_ig = product_ig >>> 8;
+        rescale_oCtan = product_oCtan >>> 8;
+    end
+
     // assign long term and short term memories
     always_ff @(posedge clk) begin
         activated_dly <= activated; 
         // long term
-        C_out_pre <= (activated_dly[f]*C_in_reg)/256 + (activated_dly[i]*activated_dly[g])/256;
+        C_out_pre <= rescale_fC[15:0] + rescale_ig[15:0];
         C_out <= C_out_pre;
         // short term
-        y_out <= (activated_dly[o]*C_out_tanh)/256;
+        y_out <= rescale_oCtan[15:0];
         x_in_valid_dly <= {x_in_valid_dly[DLY - 2 : 0], x_in_valid && ready};
     end
 
